bc8d496807ec3777a2211a209edec6beeb7c2b98
Brett Gerhardi <BGerhardi@valueretail.com>
2023-01-21 13:42:38 +0000

    prevent clearing with set and output details

--- PSEasy.Utility/Data/EnvironmentVariable/Clear-EnvironmentVariable.ps1
+++ PSEasy.Utility/Data/EnvironmentVariable/Clear-EnvironmentVariable.ps1
@@ -1,14 +1,26 @@
 function Clear-EnvironmentVariable {
     param(
-        [string]$Name
+        [string]$Name,
+        [System.EnvironmentVariableTarget]$Target
     )
-    if ([System.Environment]::GetEnvironmentVariable($Name, [System.EnvironmentVariableTarget]::User)) {
+    if ([System.Environment]::GetEnvironmentVariable($Name, [System.EnvironmentVariableTarget]::User -and
+        (   $Target -eq [System.EnvironmentVariableTarget]::User -or
+            -not $Target
+         ))) {
         [System.Environment]::SetEnvironmentVariable($Name, $null, [System.EnvironmentVariableTarget]::User)
     }
-    if ([System.Environment]::GetEnvironmentVariable($Name, [System.EnvironmentVariableTarget]::Process)) {
+
+    if ([System.Environment]::GetEnvironmentVariable($Name, [System.EnvironmentVariableTarget]::Process) -and
+    (   $Target -eq [System.EnvironmentVariableTarget]::Process -or
+        -not $Target
+     )) {
         [System.Environment]::SetEnvironmentVariable($Name, $null, [System.EnvironmentVariableTarget]::Process)
     }
-    if ([System.Environment]::GetEnvironmentVariable($Name, [System.EnvironmentVariableTarget]::Machine)) {
+
+    if ([System.Environment]::GetEnvironmentVariable($Name, [System.EnvironmentVariableTarget]::Machine) -and
+    (   $Target -eq [System.EnvironmentVariableTarget]::Machine -or
+        -not $Target
+     )) {
         [System.Environment]::SetEnvironmentVariable($Name, $null, [System.EnvironmentVariableTarget]::Machine)
     }
 
--- PSEasy.Utility/Data/EnvironmentVariable/Set-EnvironmentVariable.ps1
+++ PSEasy.Utility/Data/EnvironmentVariable/Set-EnvironmentVariable.ps1
@@ -5,8 +5,12 @@ function Set-EnvironmentVariable {
         [string]$Value,
         [System.EnvironmentVariableTarget]$Target = [System.EnvironmentVariableTarget]::User
     )
+    if ($Value.Length -eq 0) {
+        throw "Value was empty. Use Clear-EnvironmentVariable if you intend to clear it"
+    }
+
     if ((Get-EnvironmentVariable -Name $Name -Target $Target) -ne $Value) {
-        Write-Host "Setting Environment Variable $Name. " -NoNewline
+        Write-Host "Setting Environment Variable $Name. with string length $($Value.Length)" -NoNewline
         Write-Host "NOTE: These will only take effect after restart of services/console" -ForegroundColor Magenta
         [System.Environment]::SetEnvironmentVariable($Name, $Value, $Target)
     }
