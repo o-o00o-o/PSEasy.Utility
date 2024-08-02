57264382881cec37f1087251dd07785e2d61bcd0
Brett Gerhardi <BGerhardi@valueretail.com>
2024-08-02 09:14:06 +0100

    add KeyExistsBehaviour

--- PSEasy.Utility/Data/Hashtable/Add-HashtableItem.ps1
+++ PSEasy.Utility/Data/Hashtable/Add-HashtableItem.ps1
@@ -20,10 +20,21 @@ function Add-HashtableItem {
         , [alias("v")]
         [parameter(Position = 3, Mandatory = $false)]
         [object] $Value
+
+        , [alias("eb")]
+        [Parameter(Position = 4, Mandatory = $false)]
+        [VAlidateSet('LeaveExisting', 'Error', 'Overwrite')]
+        [string]$KeyExistsBehaviour = 'Overwrite'
     )
     Write-Debug "Add-HashtableItem: Checking adding $Value to $Key"
     if ($null -ne $Value -and $Value -ne '') {
         Write-Debug "Add-HashtableItem: Adding to Hashtable $Key = $Value"
-        $Hashtable.Add($Key , $Value)
+        if ($Hashtable.ContainsKey($Key)) {
+            switch ($KeyExistsBehaviour) {
+                'Overwrite' { $Hashtable.Add($Key , $Value) }
+                'Error' {throw "hashtable already has a key $Key and KeyExistsBehaviour is set to $KeyExistsBehaviour"}
+                Default {}
+            }
+        }
     }
 }
